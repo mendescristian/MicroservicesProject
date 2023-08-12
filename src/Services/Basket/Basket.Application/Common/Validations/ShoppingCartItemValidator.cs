732b83860e458587f57ae869d37024f2982ac0a6
mendescristian <cristianpmendes@gmail.com>
2023-08-12 13:57:10 -0600

    feat: Add validators to Redis entities

---
+++ src/Services/Basket/Basket.Application/Common/Validations/ShoppingCartItemValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MicroservicesProject.Core.Entities.Redis;
+
+namespace Basket.Application.Common.Validations
+{
+    public class ShoppingCartItemValidator : AbstractValidator<ShoppingCartItem>
+    {
+        public ShoppingCartItemValidator()
+        {
+            RuleFor(p => p.Quantity).NotNull().GreaterThan(0);
+            RuleFor(p => p.Color).NotNull();
+            RuleFor(p => p.Price).NotNull().GreaterThan(0);
+            RuleFor(p => p.ProductId).NotNull().NotEmpty();
+            RuleFor(p => p.ProductName).NotNull().NotEmpty();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.Application/Common/Validations/ShoppingCartValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MicroservicesProject.Core.Entities.Redis;
+
+namespace Basket.Application.Common.Validations
+{
+    public class ShoppingCartValidator : AbstractValidator<ShoppingCart>
+    {
+        public ShoppingCartValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().MaximumLength(80);
+        }
+    }
+}
